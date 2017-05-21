449d6a9861510aac2794e9fbe3c1ed8e276e70f7
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2017-05-20 22:56:31 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 """The setup.py file for Save Your Time SSH."""
 
 from setuptools import setup, find_packages
@@ -8,22 +6,17 @@ from pip.req import parse_requirements
 REQUIREMENTS = parse_requirements('requirements.txt', session='hack')
 INSTALL_REQUIRES = [str(ir.req) for ir in REQUIREMENTS]
 
-
-with open('README.md') as f:
-    README = f.read()
-
-with open('LICENSE') as f:
-    LICENSE = f.read()
-
 setup(
     name='sytssh',
     version='0.0.1',
     description='A ssh helper to save your time',
-    long_description=README,
+    long_description='''
+        Utility to manage your hosts and environments that you often need to connect
+    ''',
     author='Felipe Alexandre Rodrigues',
     author_email='felipear89@gmail.com',
     url='https://github.com/felipear89/pyssh',
-    license=LICENSE,
+    license='GNU',
     packages=find_packages(exclude=('tests', 'docs')),
     scripts=['bin/sytssh'],
     setup_requires=['pytest-runner'],
